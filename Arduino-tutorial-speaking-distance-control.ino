a8e8b9bfad222d31f5d4c9e8fb56bf6d14c1a733
Markus Laber <mlab@LMKA-MKFAZ5DTY4.united.domain>
2020-10-28 14:02:19 +0100

    Initial Commit

---
+++ Arduino-tutorial-speaking-distance-control.ino
@@ -0,0 +1,37 @@
+
+//*********************************************************************************************************
+//     Speaking distance control -- A ESP32 microcontroller measueres the distance to a object with a ultrasonic sensor,
+//     displays it on a OLED I2C display and give audio announcements by speech samples.
+//*********************************************************************************************************
+//
+// first release on 10/2020
+// Version 001 , Oct.28 2020
+//
+
+
+// THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT.
+// FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR
+// OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+//
+//
+
+// system libraries
+#include "Arduino.h"
+
+
+//**************************************************************************************************
+//                                           S E T U P                                             *
+//**************************************************************************************************
+
+void setup() {
+}
+
+
+//**************************************************************************************************
+//                                            L O O P                                              *
+//**************************************************************************************************
+
+void loop() {
+}
